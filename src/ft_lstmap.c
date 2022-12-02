4c9feb600357355a0b505801402fce55c3fed372
jkulka <kulkajens82@gmail.com>
2022-12-02 15:31:23 +0100

    Initial Commit

---
+++ src/ft_lstmap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkulka <jkulka@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 11:40:55 by jkulka            #+#    #+#             */
+/*   Updated: 2022/11/30 13:00:23 by jkulka           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*start;
+
+	if (!lst || !(*f))
+		return (NULL);
+	start = NULL;
+	new = ft_lstnew((*f)(lst->content));
+	while (lst != NULL)
+	{
+		new = ft_lstnew(f(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&start, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&start, new);
+		lst = lst->next;
+	}
+	return (start);
+}
